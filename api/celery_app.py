99f4041ee62fa93978516003dffee2a3bb42cda8
bacti <tranmhaii@@gmail.com>
2020-04-19 17:04:37 +0700

    +

--- api/celery_app.py
+++ api/celery_app.py
@@ -2,8 +2,8 @@ from celery import Celery
 
 
 config = {
-    'CELERY_BROKER_URL': 'redis://redis:6379/0',
-    'result_backend': 'redis://redis:6379/0'
+    'CELERY_BROKER_URL': 'redis://localhost:6379/0',
+    'result_backend': 'redis://localhost:6379/0'
 }
 
 celery_app = Celery('api.app', broker=config['CELERY_BROKER_URL'], include=['api.app'])
